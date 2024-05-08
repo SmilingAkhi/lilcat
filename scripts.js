a50f5f1aeefabb241a1ab097db6869f98fa21c48
SmilingAkhi <bhabdulrahaman@gmail.com>
2024-05-08 01:42:18 +0100

    lilcat

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
+  }
+
+  context.fillStyle="black";
+  context.font="20px courier";
+  score++;
+  context.fillText(score, 5, 20);
+}
+
+let isAlive = setInterval(function () {
+  // get current dino Y position
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+  // get current cactus X position
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+  // detect collision
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    // collision
+    alert("Game Over!");
+  }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
